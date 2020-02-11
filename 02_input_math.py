f4ddc2f1fbcc4ccd7d11766371194a340c65b86d
parij9435 <60633811+parij9435@users.noreply.github.com>
2020-02-11 14:43:39 +1300

    working add program

---
+++ 02_input_math.py
@@ -0,0 +1,18 @@
+# get input
+
+# ask user for name
+name = input ("what is your name? ")
+
+# ask user for two numbers
+num_1 = int (input("what is your favortie number? "))
+num_2 = int (input("what is your second favorite number? "))
+
+# add numbers together
+add = num_1 + num_2
+
+# multiply numbers together
+
+# greet user and display math
+print("hello", name)
+
+print("{} + {} = {}".format(num_1,num_2, add))
